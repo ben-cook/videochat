d5f6e663425ac5568774381f71920e7f41a24eca
Ben Cook <mcbeanoo@gmail.com>
2021-10-25 20:02:33 +1100

    rename cdk stack

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -4,7 +4,7 @@ import * as ec2 from "@aws-cdk/aws-ec2";
 import * as ecs from "@aws-cdk/aws-ecs";
 import * as iam from "@aws-cdk/aws-iam";
 
-export class CdkStack extends cdk.Stack {
+export class VideochatCdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
@@ -13,30 +13,24 @@ export class CdkStack extends cdk.Stack {
     const clientRepo = new ecr.Repository(this, "videochat-client", {
       repositoryName: "videochat-client",
     });
-    console.log(`Made repo with arn ${clientRepo.repositoryArn}`);
 
     const serverRepo = new ecr.Repository(this, "videochat-server", {
       repositoryName: "videochat-server",
     });
-    console.log(`Made repo with arn ${serverRepo.repositoryArn}`);
 
     // a Virtual Private Cloud allows for all the services we are defining to be linked together
-    // maxAzs controls the maximum number of access zones to the cloud
-    const vpc = new ec2.Vpc(this, "videochat-vpc", { maxAzs: 3 });
-    console.log(`Made VPC ${vpc.vpcId}`);
+    const vpc = new ec2.Vpc(this, "videochat-vpc");
 
     // An ecs cluster is a logical grouping of tasks or services in the cloud
     const cluster = new ecs.Cluster(this, "videochat-cluster", {
       clusterName: "videochat-cluster",
       vpc: vpc,
     });
-    console.log(`Made Cluster with arn ${cluster.clusterArn}`);
 
     const executionRole = new iam.Role(this, "videochat-execution-role", {
       assumedBy: new iam.ServicePrincipal("ecs-tasks.amazonaws.com"),
       roleName: "videochat-execution-role",
     });
-    console.log(`Made execution role with arn ${executionRole.roleArn}`);
 
     executionRole.addToPolicy(
       new iam.PolicyStatement({
@@ -61,8 +55,6 @@ export class CdkStack extends cdk.Stack {
       })
     );
 
-    console.log(`Added policies to execution role`);
-
     const TaskDefinition = new ecs.FargateTaskDefinition(
       this,
       "videochat-task-definition",
@@ -71,8 +63,6 @@ export class CdkStack extends cdk.Stack {
         family: "videochat-task-definition",
       }
     );
-
-    console.log(`Created task definiton`);
 
     // Add All Containers
     TaskDefinition.addContainer("client", {
@@ -88,14 +78,10 @@ export class CdkStack extends cdk.Stack {
       portMappings: [{ containerPort: 9000 }],
     });
 
-    console.log(`Added containers to task definition`);
-
     new ecs.FargateService(this, "videochat", {
       cluster: cluster,
       taskDefinition: TaskDefinition,
       serviceName: "videochat-task-definition",
     });
-
-    console.log(`Created fargate service`);
   }
 }
